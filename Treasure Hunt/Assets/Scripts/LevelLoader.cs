0d55b7c95d2a2a15c6d11ce543538660246d8266
apflour <erics234@nycstudents.net>
2024-11-10 22:39:26 -0500

    load all levels +  standstill screens

--- Treasure Hunt/Assets/Scripts/LevelLoader.cs
+++ Treasure Hunt/Assets/Scripts/LevelLoader.cs
@@ -5,29 +5,34 @@ using UnityEngine.SceneManagement;
 public class LevelLoader : MonoBehaviour
 {
     // Called when we click the "Level 1" button.
-    public void OnHelpButton ()
+    public void OnLevel1Button ()
     {
         SceneManager.LoadScene(3);
     }    
 
     // Called when we click the "Level 2" button.
-    public void OnHelpButton ()
+    public void OnLevel2Button ()
     {
         SceneManager.LoadScene(4);
     }    
 
     // Called when we click the "Level 3" button.
-    public void OnHelpButton ()
+    public void OnLevel3Button ()
     {
         SceneManager.LoadScene(5);
     }    
 
 
     // Called when we click the "Level 4" button.
-    public void OnHelpButton ()
+    public void OnLevel4Button ()
     {
         SceneManager.LoadScene(6);
     }    
 
-
+    // Called when we click the "Back" button.
+    public void OnBackButton ()
+    {
+        SceneManager.LoadScene(0);
+    }
+    
 }
