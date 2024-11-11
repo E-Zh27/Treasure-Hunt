727453db6b32959558b8145747d34be1c3537aa4
apflour <erics234@nycstudents.net>
2024-11-10 21:33:38 -0500

    levels

---
+++ Treasure Hunt/Assets/Scripts/LevelLoader.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelLoader : MonoBehaviour
+{
+    // Called when we click the "Level 1" button.
+    public void OnHelpButton ()
+    {
+        SceneManager.LoadScene(3);
+    }    
+
+    // Called when we click the "Level 2" button.
+    public void OnHelpButton ()
+    {
+        SceneManager.LoadScene(4);
+    }    
+
+    // Called when we click the "Level 3" button.
+    public void OnHelpButton ()
+    {
+        SceneManager.LoadScene(5);
+    }    
+
+
+    // Called when we click the "Level 4" button.
+    public void OnHelpButton ()
+    {
+        SceneManager.LoadScene(6);
+    }    
+
+
+}
